0317bac5f22799cb8715551a01685efd10e26b40
wtg <wangtiegang3@jd.com>
2021-03-25 11:47:09 +0800

    单元测试获取设备信息

--- app/src/androidTest/java/com/github/gang/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/github/gang/ExampleInstrumentedTest.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.github.gang.utils.DeviceUtils;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +20,17 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
+    private Context context;
+
+    @Before
+    public void setUp() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.github.gang", appContext.getPackageName());
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    }
+
+    @Test
+    public void checkDevice(){
+        assertNotNull(DeviceUtils.getAndroidId(context));
+        assertNotNull(DeviceUtils.getMac(context));
     }
 }
---
+++ app/src/main/java/com/github/gang/utils/DeviceUtils.java
@@ -0,0 +1,78 @@
+package com.github.gang.utils;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class DeviceUtils {
+    public static String getAndroidId(Context context){
+        return Settings.Secure.getString(context.getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+    }
+
+    public static String getMac(Context paramContext) {
+        try {
+            if (Build.VERSION.SDK_INT >= 23) {
+                String str = getMacMoreThanM(paramContext);
+                if (!TextUtils.isEmpty(str)) {
+                    return str;
+                }
+            }
+            // 6.0以下手机直接获取wifi的mac地址即可
+            WifiManager wifiManager = (WifiManager) paramContext.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (wifiInfo != null) {
+                return wifiInfo.getMacAddress();
+            }
+        } catch (Throwable throwable) {
+        }
+        return null;
+    }
+
+    /**
+     * android 6.0+获取wifi的mac地址
+     *
+     * @param paramContext
+     * @return
+     */
+    private static String getMacMoreThanM(Context paramContext) {
+        try {
+            //获取本机器所有的网络接口
+            Enumeration enumeration = NetworkInterface.getNetworkInterfaces();
+            while (enumeration.hasMoreElements()) {
+                NetworkInterface networkInterface = (NetworkInterface) enumeration.nextElement();
+                //获取硬件地址，一般是MAC
+                byte[] arrayOfByte = networkInterface.getHardwareAddress();
+                if (arrayOfByte == null || arrayOfByte.length == 0) {
+                    continue;
+                }
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (byte b : arrayOfByte) {
+                    //格式化为：两位十六进制加冒号的格式，若是不足两位，补0
+                    stringBuilder.append(String.format("%02X:", new Object[]{Byte.valueOf(b)}));
+                }
+                if (stringBuilder.length() > 0) {
+                    //删除后面多余的冒号
+                    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+                }
+                String str = stringBuilder.toString();
+                // wlan0:无线网卡 eth0：以太网卡
+                if (networkInterface.getName().equals("wlan0")) {
+                    return str;
+                }
+            }
+        } catch (SocketException socketException) {
+            return null;
+        }
+        return null;
+    }
+
+}
