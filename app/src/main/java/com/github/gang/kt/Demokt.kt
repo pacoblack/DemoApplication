ff31a50530036ff89ea019928d6e2c4e7edd651e
wtg <wangtiegang3@jd.com>
2021-05-18 18:42:37 +0800

    kt协程

--- app/src/main/java/com/github/gang/kt/Demokt.kt
+++ app/src/main/java/com/github/gang/kt/Demokt.kt
@@ -1,2 +1,42 @@
 package com.github.gang.kt
 
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+fun setUpUI(){
+    GlobalScope.launch(Main) {
+        val dataDeferred  = requestDataAsync()
+        doSomethingElse()
+        val data = dataDeferred.await()
+        processData(data)
+    }
+    Thread.sleep(1000)
+    doSomethingElse2()
+}
+
+fun requestDataAsync(): Deferred<String> {
+    // 启动一个异步协程去执行耗时任务
+    return GlobalScope.async {
+        requestData()
+    }
+}
+
+fun requestData(): String {
+    Thread.sleep(2000)
+    return "Ui Data"
+}
+
+fun doSomethingElse(){
+    println("doSomethingElse")
+}
+
+fun doSomethingElse2(){
+    println("doSomethingElse2")
+}
+
+fun processData(data: String) {
+    println("updateUI$data")
+}
